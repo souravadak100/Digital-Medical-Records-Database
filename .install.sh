f37ffd351d982ace0771aba6448bd1390a7f65ee
souravadak100 <souravadak100@gmail.com>
2025-04-05 22:03:13 +0530

    Added .gitignore and install.sh script for one-time package installation for Backend and Frontend

---
+++ .install.sh
@@ -0,0 +1,7 @@
+py -m pip install selenium
+py -m pip install django
+py -m pip install djangorestframework
+py -m pip install django-cors-headers
+py -m pip install pyjwt
+cd React-FrontEnd/
+npm install
